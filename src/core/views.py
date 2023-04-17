1f8de47141cc68d19859159a90beef69deab0433
lucky <luckyetefia@gmail.com>
2023-04-16 18:20:50 -0700

    edited some files to match with remote

--- src/core/views.py
+++ src/core/views.py
@@ -2,9 +2,8 @@ from . models import Profile
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-from django.views.generic.edit import FormView 
+from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
-from .mixins.EmailActivationMixin import EmailActivationMixin
 from .forms import (UserRegistrationForm, SupervisorRegistrationForm, CadetRegistrationForm)
 #from .token_generator import account_activation_token
 from django.contrib.auth.decorators import login_required
@@ -12,31 +11,31 @@ from django.contrib.auth.decorators import login_required
 
 class Register(FormView):
     template_name = 'core/signup.html'
-    form_class = UserRegistrationForm 
+    form_class = UserRegistrationForm
     success_url = reverse_lazy("core:signup2")
-         
+
     def form_valid(self, form):
-        self.request.session['user_data'] = self.request.POST 
-        return super().form_valid(form) 
+        self.request.session['user_data'] = self.request.POST
+        return super().form_valid(form)
 
 
-    
-class Register2(EmailActivationMixin, FormView):
-    custom_text_value = str() 
+
+class Register2(FormView):
+    custom_text_value = str()
     template_name = 'core/signup2.html'
-    success_url = reverse_lazy("core:signup_completed")    
-    
+    success_url = reverse_lazy("core:signup_completed")
+
     def get_form_class(self):
         if self.request.session['user_data'].get('account_type').upper() == 'Cadet':
             self.form_class = CadetRegistrationForm
             self.custom_text_value = "Cadet No"
-              
+
         else:
-            self.form_class = SupervisorRegistrationForm 
+            self.form_class = SupervisorRegistrationForm
             self.custom_text_value = "Supervisor No"
-            
+
         return super().get_form_class()
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['email'] = self.request.session['user_data'].get('email')
@@ -44,7 +43,7 @@ class Register2(EmailActivationMixin, FormView):
         context['form'] = self.get_form()
         context['custom_text_value'] = self.custom_text_value
         return context
-    
+
     def form_valid(self, form):
         email = self.request.session['user_data'].get('email')
         username = form.cleaned_data['username']
@@ -56,32 +55,32 @@ class Register2(EmailActivationMixin, FormView):
         profile = Profile()
         profile.user = user
         profile.account_type = self.request.session['user_data'].get('account_type')
-        profile.gender = self.request.session['user_data'].get('gender')            
+        profile.gender = self.request.session['user_data'].get('gender')
         user.save()
         profile.save()
         return super().form_valid(form)
        # self.init_email_activation(email)
-       # self.send_activation_mail() 
-        
-        
+       # self.send_activation_mail()
+
+
 class  SignUpCompleted(TemplateView):
     template_name = 'core/signup_completed.html'
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context['email'] = self.request.session['user_data'].get('email')
         return context
 
 
-@login_required    
+@login_required
 def dashboard(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
 
     if profile.account_type == 'supervisor':
         return render(request, 'core/supervisor_admin_dashboard.html', {
             'profile' : profile
         })
-    
+
     else:
         return render(request, 'core/cadet_admin_dashboard.html', {
             'profile' : profile
@@ -90,7 +89,7 @@ def dashboard(request):
 
 @login_required
 def settings(request):
-    profile = get_object_or_404(Profile, user=request.user) 
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/settings.html', {
         'profile': profile
     })
@@ -102,59 +101,55 @@ def lockscreen(request):
 @login_required
 def cadets(request):
     profile = get_object_or_404(Profile, user=request.user)
-    cadets = Profile.objects.filter(account_type='cadet')  
     return render(request, 'core/cadets.html', {
-        'profile':profile,
-        'cadets' : cadets 
+        'profile':profile
     })
 
 @login_required
-def cadet_profile(request, id):
-    profile = get_object_or_404(Profile, user=request.user)  
+def cadet_profile(request):
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/profile.html', {
         'profile':profile
     })
 
 @login_required
 def cadet_messages(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/message.html', {
         'profile':profile
     })
 
 @login_required
 def cadet_taskboard(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/taskboard.html', {
         'profile':profile
     })
 
 @login_required
 def cadet_logbook(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/cadet_logbook.html', {
         'profile': profile
     })
 
 @login_required
 def cadet_logbook_overview(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/cadetlogbook_overview.html', {
         'profile': profile
     })
 
 @login_required
 def supervisors(request):
-    profile = get_object_or_404(Profile, user=request.user)  
-    supervisors = Profile.objects.filter(account_type='supervisor')  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/supervisors.html', {
-        'profile': profile,
-        'supervisors' : supervisors
+        'profile': profile
     })
 
 @login_required
 def supervisor_profile(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/supervisor_profile.html', {
         'profile': profile
     })
@@ -162,7 +157,7 @@ def supervisor_profile(request):
 
 @login_required
 def timeline(request):
-    profile = get_object_or_404(Profile, user=request.user)  
+    profile = get_object_or_404(Profile, user=request.user)
     return render(request, 'core/timeline.html', {
         'profile':profile
     })
